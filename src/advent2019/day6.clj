69038cf59de9bedf7a0d4896c12c4cf7ae9d3789
Sergey Trofimov <sarg@sarg.org.ru>
2019-12-06 17:39:41 +0100

    Day 6. Bonus - find a way to santa in graph.

--- src/advent2019/day6.clj
+++ src/advent2019/day6.clj
@@ -1,10 +1,26 @@
 (ns advent2019.day6
   (:require [clojure.java.io :as io]
+            [clojure.core.reducers :as r]
             [clojure.set :as s]))
 
 (def data
   (with-open [rdr (io/reader (io/resource "day6.in"))]
     (into [] (map #(into [] (.split % "\\)")) (line-seq rdr)))))
+
+(def test-data
+  [["COM" "B"]
+   ["B" "C"]
+   ["C" "D"]
+   ["D" "E"]
+   ["E" "F"]
+   ["B" "G"]
+   ["G" "H"]
+   ["D" "I"]
+   ["E" "J"]
+   ["J" "K"]
+   ["K" "YOU"]
+   ["I" "SAN"]
+   ["K" "L"]])
 
 (defn adj-graph [vs]
   (loop [vs vs
@@ -15,17 +31,11 @@
     (if (empty? vs)
       [(persistent! acc) (s/difference (persistent! roots) (persistent! not-roots))]
 
-      ;; p0 ) p1
-      ;; p1 ) p2
-      ;;
-      ;; { p1 [ ... p0 ], p0 [ ... ]}
-      ;; { p2 [ ... p1 ], p1 [ ... ]}
       (let [[p1 p2] (first vs)
-            p1-list (get acc p1 [])
-            p2-list (get acc p2 [])]
-
+            p1-list (get acc p1 [])]
+        
         (recur (drop 1 vs)
-               (conj! acc {p2 p2-list, p1 (conj p1-list p2)})
+               (conj! acc {p1 (conj p1-list p2)})
                (conj! roots p1)
                (conj! not-roots p2))))))
 
@@ -38,21 +48,48 @@
 
       (if (empty? layer) total
           (recur
-           (set (flatten (map #(get graph %) layer)))
+           (set (flatten (map #(get graph % []) layer)))
            (inc level)
            (+ total (* (count layer) level)))))))
 
-(assert (= 42 (solution
-               [["COM" "B"]
-                ["B" "C"]
-                ["C" "D"]
-                ["D" "E"]
-                ["E" "F"]
-                ["B" "G"]
-                ["G" "H"]
-                ["D" "I"]
-                ["E" "J"]
-                ["J" "K"]
-                ["K" "L"]])))
+(assert (= 42 (solution test-data)))
 
 (assert (= 241064 (solution data)))
+
+(defn adj-bonus-graph [vs]
+  (loop [vs vs
+         acc (transient {})]
+    
+    (if (empty? vs)
+      (persistent! acc)
+
+      (let [[p1 p2] (first vs)
+            p1-list (get acc p1 [])
+            p2-list (get acc p2 [])]
+
+        (recur (drop 1 vs)
+               (conj! acc {p2 (conj p2-list p1)}))))))
+
+(defn reachable [graph from]
+  (loop [acc (transient {})
+         step 0
+         from from]
+
+    (if (empty? from) (persistent! acc)
+        (let [new-from (flatten (map #(get graph % []) from))]
+          (recur (conj! acc (zipmap from (repeat step)))
+                 (inc step)
+                 new-from)))))
+
+(defn bonus [vs]
+  (let [graph (adj-bonus-graph vs)
+        you (reachable graph ["YOU"])
+        san (reachable graph ["SAN"])
+
+        common (s/intersection (into #{} (keys you))
+                               (into #{} (keys san)))]
+
+    (first (sort (map #(+ -2 (get you %) (get san %)) common)))))
+
+(assert (= 4 (bonus test-data)))
+(assert (= 418 (bonus data)))

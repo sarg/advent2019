54d9e111224486d8782af7ace7fa9ad08a483078
Sergey Trofimov <sarg@sarg.org.ru>
2019-12-08 06:51:14 +0100

    Day 8. Bonus - merge layers.

--- src/advent2019/day8.clj
+++ src/advent2019/day8.clj
@@ -1,13 +1,11 @@
 (ns advent2019.day8
   (:require [clojure.java.io :as io]
-            [advent2019.day5 :refer [digits]]
-            [clojure.math.combinatorics :as cmb]))
+            [clojure.string :as s]))
 
 
 (def data
   (with-open [rdr (io/reader (io/resource "day8.in"))]
     (into [] (.readLine rdr))))
-
 
 ;; The image you received is 25 pixels wide and 6 pixels tall.
 
@@ -18,10 +16,40 @@
 (defn layers [data x y]
   (partition (* x y) data))
 
-(def layer 
-  (first (sort (fn [c1 c2] (compare (count (filter #(= \0 %) c1))
-                                   (count (filter #(= \0 %) c2))))
-              (layers data 25 6))))
+(def width 25)
+(def height 6)
+(def image-layers (layers data width height))
+(def layer-with-fewest-0
+  (first (sort-by (fn [c] (count (filter #(= \0 %) c)))
+                  image-layers)))
 
-(* (count (filter #(= \1 %) layer))
-   (count (filter #(= \2 %) layer)))
+(assert (= 2210
+           (* (count (filter #(= \1 %) layer-with-fewest-0))
+              (count (filter #(= \2 %) layer-with-fewest-0)))))
+
+;; The image is rendered by stacking the layers and aligning the pixels with the
+;; same positions in each layer. The digits indicate the color of the
+;; corresponding pixel: 0 is black, 1 is white, and 2 is transparent.
+
+;; So, if a given position has a transparent pixel in the first and second
+;; layers, a black pixel in the third layer, and a white pixel in the fourth
+;; layer, the final image would have a black pixel at that position.
+
+(defn reduce-layers [l1 l2]
+  (map #(if (= %1 \2) %2 %1) l1 l2))
+
+(println 
+ (->>
+  image-layers
+  (reduce reduce-layers)
+  (partition width)
+  (map #(replace {\0 " "} %))
+  (map #(apply str %))
+  (s/join "\n")))
+
+;;  11   11  1111  11  1111 
+;; 1  1 1  1 1    1  1 1    
+;; 1    1    111  1    111  
+;; 1    1 11 1    1 11 1    
+;; 1  1 1  1 1    1  1 1    
+;;  11   111 1111  111 1111 

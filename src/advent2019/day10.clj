c78575379d145a3b16dfd4084e53ad757cd0800a
Sergey Trofimov <sarg@sarg.org.ru>
2019-12-11 09:01:17 +0100

    Use interleave-all for day10.2

--- src/advent2019/day10.clj
+++ src/advent2019/day10.clj
@@ -195,5 +195,37 @@
 
     (+ yy (* 100 xx))))
 
+;; taken from https://github.com/weavejester/medley/blob/1.1.0/src/medley/core.cljc#L198
+(defn interleave-all
+  "Returns a lazy seq of the first item in each coll, then the second, etc.
+  Unlike `clojure.core/interleave`, the returned seq contains all items in the
+  supplied collections, even if the collections are different sizes."
+  {:arglists '([& colls])}
+  ([] ())
+  ([c1] (lazy-seq c1))
+  ([c1 c2]
+   (lazy-seq
+    (let [s1 (seq c1), s2 (seq c2)]
+      (if (and s1 s2)
+        (cons (first s1) (cons (first s2) (interleave-all (rest s1) (rest s2))))
+        (or s1 s2)))))
+  ([c1 c2 & colls]
+   (lazy-seq
+    (let [ss (remove nil? (map seq (conj colls c2 c1)))]
+      (if (seq ss)
+        (concat (map first ss) (apply interleave-all (map rest ss))))))))
+
+(defn bonus-i [data x y n]
+  (->> (with-angles data x y)
+       (group-by first)
+       (into (sorted-map))
+       (vals)
+       (apply interleave-all)
+       (drop (dec n))
+       (first)
+       ((fn [[_ _ x y]] (+ y (* 100 x))))))
+
 (assert (= 802 (bonus tst1 11 13 200)))
 (assert (= 416 (bonus data 37 25 200)))
+
+(assert (= 416 (bonus-i data 37 25 200)))

613fc9a1aef8e53a9cab2afa33ee0dca7b57edbe
Sergey Trofimov <sarg@sarg.org.ru>
2019-12-12 19:57:57 +0100

    Day 12. Finish C solution for part 2.

--- src/advent2019/day12.c
+++ src/advent2019/day12.c
@@ -1,76 +1,62 @@
+#include <string.h>
 #include <stdio.h>
 
+#define ulong unsigned long
 #define CMP(a,b) (a>b?-1:a==b?0:1)
-#define MCMP(x,a,b,c,d) (CMP(x[a],x[b])+CMP(x[a],x[c])+CMP(x[a],x[d]))
+#define MCMP(x,a,b,c,d) CMP(x[a],x[b])+CMP(x[a],x[c])+CMP(x[a],x[d])
+#define gravity(x, v) \
+    v[0] += MCMP(x, 0, 1, 2, 3); \
+    v[1] += MCMP(x, 1, 0, 2, 3); \
+    v[2] += MCMP(x, 2, 1, 0, 3); \
+    v[3] += MCMP(x, 3, 1, 2, 0); \
 
-__attribute__((always_inline))
-void gravity(long x[], long v[]) {
-    v[0] += MCMP(x, 0, 1, 2, 3);
-    v[1] += MCMP(x, 1, 0, 2, 3);
-    v[2] += MCMP(x, 2, 1, 0, 3);
-    v[3] += MCMP(x, 3, 1, 2, 0);
-}
-
-__attribute__((always_inline))
-void velocity(long x[], long v[]) {
-    x[0] += v[0];
-    x[1] += v[1];
-    x[2] += v[2];
+#define velocity(x, v) \
+    x[0] += v[0]; \
+    x[1] += v[1]; \
+    x[2] += v[2]; \
     x[3] += v[3];
+
+#define GV(x,v) gravity(x,v);velocity(x,v);
+#define EQ(a,b) a[0]==b[0] && a[1] == b[1] && a[2] == b[2] && a[3] == b[3]
+#define PP(x) for (int p = 0; p < 4; p++) { printf("% 3ld", x[p]); } printf("\n");
+
+ulong cycle(long x[]) {
+    long v[4] = { 0, 0, 0 };
+    long v2[4] = { 0, 0, 0 };
+
+    long x2[4]; memcpy(x2, x, 4 * sizeof(ulong));
+
+    ulong i = 0;
+    do {
+        GV(x, v);
+        GV(x2, v2);GV(x2, v2);
+
+        i++;
+    } while(!(EQ(x, x2) && EQ(v, v2)));
+
+    return i;
 }
 
-#define GV(t) gravity(m ## t, v ## t);velocity(m ## t, v ## t);
-#define STEP(i) GV(x ## i) GV(y ## i) GV(z ## i)
-#define EQ(a,b) a[0]==b[0] && a[1] == b[1] && a[2] == b[2] && a[3] == b[3]
-#define TEQ(t) EQ(t ## x ## 1, t ## x ## 2) && EQ(t ## y ## 1, t ## y ## 2) && EQ(t ## z ## 1, t ## z ## 2)
-#define ALLEQ TEQ(m) && TEQ(v)
+ulong gcd(ulong a, ulong b){ 
+    if (a == 0) 
+        return b;  
+    return gcd(b % a, a);  
+ } 
+  
+   
+// Function to return LCM of two numbers  
+ulong lcm(ulong a, ulong b) {  
+    return (a*b)/gcd(a, b);  
+}
 
 int main() {
-    long mx1[4] = { -8, -5, 11, 1};
-    long my1[4] = { -9, 2, 8, -4};
-    long mz1[4] = { -7, -1, -14, -11};
+    long mx[4] = { -8, -5, 11, 1};
+    long my[4] = { -9, 2, 8, -4};
+    long mz[4] = { -7, -1, -14, -11};
 
-    /* long mx1[4] = { -1, 2, 4, 3 }; */
-    /* long my1[4] = { 0, -10, -8, 5 }; */
-    /* long mz1[4] = { 2, -7, 8, -1 }; */
+    ulong cx = cycle(mx);
+    ulong cy = cycle(my);
+    ulong cz = cycle(mz);
 
-    long vx1[4] = { 0, 0, 0};
-    long vy1[4] = { 0, 0, 0};
-    long vz1[4] = { 0, 0, 0};
-
-    /* long mx2[4] = { -1, 2, 4, 3 }; */
-    /* long my2[4] = { 0, -10, -8, 5 }; */
-    /* long mz2[4] = { 2, -7, 8, -1 }; */
-
-    long mx2[4] = { -8, -5, 11, 1};
-    long my2[4] = { -9, 2, 8, -4};
-    long mz2[4] = { -7, -1, -14, -11};
-    long vx2[4] = { 0, 0, 0};
-    long vy2[4] = { 0, 0, 0};
-    long vz2[4] = { 0, 0, 0};
-
-    unsigned long i = 0;
-
-    do {
-        STEP(1);
-        STEP(2);
-        STEP(2);
-        i++;
-        if (i % 100000 == 0) {
-           printf("%lu\n", i);
-        }
-    } while (!(ALLEQ));
-
-    /* for(int i=0; i<10; i++) { */
-    /*     printf("Step %d\n", i); */
-    /*     for(int x=0; x<4; x++) { */
-    /*         printf("pos=% 3ld % 3ld % 3ld vel=% 3ld % 3ld % 3ld\n", mx1[x], my1[x], mz1[x], vx1[x], vy1[x], vz1[x]); */
-    /*     } */
-    /*     printf("\n"); */
-
-    /*     STEP(1); */
-
-    /* } */
-
-    printf("%lu\n", i);
+    printf("lcm(%lu, %lu, %lu) = %lu\n", cx, cy, cz, lcm(lcm(cx, cy), cz));
 }

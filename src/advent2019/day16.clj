2e3d35d34665b905623fac4b7f4b3c22b1d21c1f
Sergey Trofimov <sarg@sarg.org.ru>
2019-12-16 08:18:27 +0100

    Day 16. Tricky bonus.

--- src/advent2019/day16.clj
+++ src/advent2019/day16.clj
@@ -27,18 +27,23 @@
 (definline last-digit [n] `(mod (Math/abs ~n) 10))
 
 (defn fft-step [in idx & _]
-  (->> in
-       (map * (map (partial pattern-n* (inc idx)) (range 1 (inc (count in)))))
-       (reduce +)
-       (last-digit)))
+  (loop [in in
+         i 1
+         acc 0]
+
+    (if (empty? in) (last-digit acc)
+        (let [el (first in)
+              p (pattern-n* (inc idx) i)]
+          (recur (rest in) (inc i)
+                 (+ acc (* el p)))))))
 
 (defn fft [in] (map-indexed (partial fft-step in) in))
 
-(defn solution [x & [n]]
+(defn solution [x]
   (->> x
-       (digit-str-to-vec)
+       (map #(- (int %) (int \0)))
        (iterate fft)
-       (drop (or n 100))
+       (drop 100)
        (first)
        (take 8)
        (s/join)))
@@ -48,3 +53,111 @@
 (assert (= "52432133" (solution "69317163492948606335995924319873")))
 ;; (assert (= "84970726" (solution data)))
 
+;; The real signal is your puzzle input repeated 10000 times. Treat this new
+;; signal as a single input list. Patterns are still calculated as before, and
+;; 100 phases of FFT are still applied.
+
+;; The first seven digits of your initial input signal also represent the
+;; message offset.
+(def offset
+  (Integer/parseInt (.substring data 0 7)))
+
+;; The message offset is the location of the eight-digit message in the final
+;; output list. Specifically, the message offset indicates the number of digits
+;; to skip before reading the eight-digit message. For example, if the first
+;; seven digits of your initial input signal were 1234567, the eight-digit
+;; message would be the eight digits after skipping 1,234,567 digits of the
+;; final output list. Or, if the message offset were 7 and your final output
+;; list were 98765432109876543210, the eight-digit message would be
+;; 21098765. (Of course, your real message offset will be a seven-digit number,
+;; not a one-digit number like 7.)
+
+
+;; After repeating your input signal 10000 times and running 100 phases of FFT,
+;; what is the eight-digit message embedded in the final output list?
+
+;; d zeros
+;; d+1 ones
+;; d+1 zeros
+;; d+1 minus ones
+
+(defn partial-sum [cumsums len from to]
+  (- (cumsums (min len to)) (cumsums from)))
+
+(defn fft1 [s]
+  (let [sums
+        (into [] (reductions + 0 (map #(- (int %) 48) s)))
+
+        s-len (count s)
+
+        dest (byte-array s-len)]
+
+    (doseq [d (range s-len)
+            :let [p (* 4 (inc d))
+
+                  ones
+                  (map #(partial-sum sums s-len % (+ % d 1))
+                       (range d s-len p))
+
+                  minus-ones
+                  (map #(partial-sum sums s-len % (+ % d 1))
+                       (range (+ d d d 1 1) s-len p))]]
+
+      (aset-byte dest d
+                 (+ 48
+                    (last-digit
+                     (- (reduce + ones)
+                        (reduce + minus-ones))))))
+    (String. dest)))
+
+(defn solution-fast [x]
+  (subs (first (drop 100 (iterate fft1 x)))
+        0 8))
+
+(assert (= "84970726" (solution-fast data)))
+
+;; Given OFFSET and SIGNAL * 10000
+;; Return part of signal from OFFSET till end
+(defn from-offset [signal offset times]
+  ;; first skip X signals from beginning
+  ;; and get reminder
+  ;; xxx xxx xxx xx|x ... xxx xxx
+  ;;            ^  ^
+  ;;         skip  offset
+  ;; 
+  (let [s-len (count signal)
+        s-rem (rem offset s-len)
+        s-quot (quot offset s-len)]
+
+    (s/join
+     [(subs signal s-rem)
+      (s/join (repeat (- times s-quot 1) signal))])))
+
+(defn cumulative-sum [s]
+  (loop [l (dec (count s)) acc s]
+    (if (neg? l) acc
+        (recur (dec l)
+               (assoc! acc l
+                       (mod (+ (get acc (inc l) 0)
+                               (get acc l))
+                            10))))))
+
+(defn bonus [s]
+  (let [offset (Integer/parseInt (subs s 0 7))
+
+        signal
+        (->>
+         (from-offset s offset 10000)
+         (map #(- (int %) (int \0)))
+         (into []))]
+    
+    (loop [i 0 signal (transient signal)]
+      (if (= i 100)
+        (s/join (take 8 (persistent! signal)))
+
+        (recur (inc i) (cumulative-sum signal))))))
+
+(assert (= "84462026" (bonus "03036732577212944063491565474664")))
+(assert (= "78725270" (bonus "02935109699940807407585447034323"))) 
+(assert (= "53553731" (bonus "03081770884921959731165446850517"))) 
+;; (assert (= "47664469" (bonus data)))
